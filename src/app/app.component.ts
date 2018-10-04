e0c87ccafb63468d5ef058c3085c59503e90cb59
chingaloEric <echingalo@gmail.com>
2018-10-04 18:31:19 +0300

    getHtml()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,26 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public formGroup : any;
-  // public editorContents: string = 'Hello World';
-
   public options: Object = {
-    placeholderText: 'Edit Your Content Here!',
-    charCounterCount: true,
-    disableRightClick: false,
-    colorsBackground : '#61BD6D',
-    editorClass: 'custom-class',
-    theme: "dark",
-   //multiLine: false,
-    //immediateAngularModelUpdate : true,
-    // events : {
-    //   'froalaEditor.focus' : function(e, editor) {
-    //     console.log('Contents:',this.editorContents);
-    //   }
-    // }
-  }
-
-  onSubmit(){
-      console.log
+    placeholder: "Write your comment",
+    events : {
+      'froalaEditor.keyup' : function(e, editor) {
+        //html.get() gets the html from the editor
+        console.log(editor.html.get());
+      }
+    }
   }
 }
