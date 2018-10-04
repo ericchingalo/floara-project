f1ffb0888ef44cc7df73c157c19c4804818cd3ea
chingaloEric <echingalo@gmail.com>
2018-10-04 14:14:08 +0300

    added options to the texteditor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  public options: Object = {
+    placeholderText: 'Edit Your Content Here!',
+    charCounterCount: true
+  }
+
   title = 'floara-project';
 }
